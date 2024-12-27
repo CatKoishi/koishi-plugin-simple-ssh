1f2a095e07b101851c4e07912ee337f887fcde55
CatKoishi <1194703727@qq.com>
2024-12-27 17:39:47 +0800

    release: 0.1.0 初次发布

--- readme.md
+++ readme.md
@@ -1,5 +1,40 @@
-# koishi-plugin-simple-ssh
+<!-- markdownlint-disable MD026 MD028 MD033 MD041 -->
+
+<div align="center">
+  <a href="https://koishi.chat/" target="_blank">
+    <img width="160" src="https://koishi.chat/logo.png" alt="logo">
+  </a>
+
+<h1><ruby>koishi-plugin-simple-ssh<rp>(</rp><rt>简易SSH功能</rt><rp>)</ruby></h1>
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-simple-ssh?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-simple-ssh)
 
-use ssh to run simple command on linux
+</div>
+
+## ⚠️SSH功能涉及服务器安全问题，请务必对指令权限进行严格管理
+
+## ⚠️本插件仅供学习参考，任何直接或间接由本插件造成的损失，插件本人概不负责
+
+## 📖 介绍
+
+在Koishi上使用SSH执行简易的指令
+
+本人纯编程白痴，只会Ctrl C/V，有bug请见谅qwq
+
+## 📦 功能
+
+- [x] 查看录入的SSH服务器列表
+- [x] 连接SSH并执行指令
+- [ ] 快捷执行指令
+
+功能说明请使用`help ssh`获取
+
+连接SSH后发送的任何消息都将被视为SSH输入指令，取消连接可使用 `exit` 或在60s内无任何输入
+
+---
+
+## 📝 更新历史
+
+### 0.1.0
+
+- 初次发布，支持SSH列表与SSH连接
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,91 @@
 import { Context, Schema } from 'koishi'
+import { NodeSSH, Config as ConfigSSH, SSHExecCommandResponse } from 'node-ssh'
 
 export const name = 'simple-ssh'
 
-export interface Config {}
+export const usage = `
+# ⚠️SSH功能涉及服务器安全问题，请务必对指令权限进行严格管理
+# ⚠️本插件仅供学习参考，任何由本插件造成的损失，插件本人概不负责
 
-export const Config: Schema<Config> = Schema.object({})
+提供十分简易的远程连接SSH执行指令的功能
 
-export function apply(ctx: Context) {
+连接SSH之后发送 exit 退出
+
+## ☎️联系方式
+Github提issue | QQ：1194703727 | nyakoishi@qq.com
+`
+
+type ServerList = ConfigSSH & {
+  nickname: string;
+}
+
+export interface Config {
+  servList: ServerList[];
+}
+
+export const Config: Schema<Config> = Schema.object({
+  servList: Schema.array(Schema.object({
+    nickname: Schema.string().default('XX云').description('服务器昵称'),
+    host: Schema.string().default('127.0.0.1').description('服务器IP'),
+    port: Schema.number().default(22).min(1).max(65535).description('SSH端口'),
+    username: Schema.string().description('用户名'),
+    password: Schema.string().role('secret').description('密码')
+  })).role('table').description('SSH服务器列表')
+})
+
+export function apply(ctx: Context, config: Config) {
   // write your plugin here
+  ctx.command('ssh', 'SSH功能', {authority: 4})
+
+  ctx.command('ssh.list', '查看SSH服务器列表')
+  .action( () => {
+    let str = ''
+    for(let i = 0; i < config.servList.length; i++) {
+      str += `${i+1}. ${config.servList[i].nickname}\r\n`
+    }
+    return str;
+  })
+
+  ctx.command('ssh.connect <server:string>', '连接SSH并执行指令')
+  .action(async ({session}, server) => {
+    const regServ = /^[1-9]\d*(f|F|服)$/;
+    if(!regServ.test(server))
+      return '请检查服务器编号是否为：编号f (12f)'
+
+    let index:number = Number(server.substring(0,server.length-1));
+    const maxServNum = config.servList.length;
+
+    if(index > maxServNum || index < 1)
+      return '没有这个服务器呢'
+
+    const ssh = new NodeSSH();
+    let cdir = ''
+    try {
+      await ssh.connect(config.servList[index-1]);
+      const test = await ssh.execCommand('pwd');
+      cdir = test.stdout;
+      session.send(`成功连接到 ${config.servList[index-1].nickname}`)
+    } catch (err) {
+      return 'SSH连接错误'
+    }
+
+    let exit = false;
+    let res: SSHExecCommandResponse;
+    for(;!exit;) {
+      await session.prompt(60000).then( async(cmd) => {
+        if(!cmd || cmd === 'exit') {
+          await ssh.dispose();
+          await session.send('因超时或用户主动退出')
+          exit = true;
+          return;
+        } else if(/^cd \S+$/.test(cmd)) { // cd
+          cdir = cmd.substring(3, cmd.length);
+        } else {
+          res = await ssh.execCommand(cmd, { cwd: cdir });
+          session.send(res.stdout);
+        }
+      })
+    }
+    
+  })
 }
